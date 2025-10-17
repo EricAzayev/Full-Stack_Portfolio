38b148e88cf3b6612292c297084b6754a7d33b53
EricAzayev <azayeveric@gmail.com>
2025-10-16 23:32:44 -0400

    Implemented Create,Update,Delete

---
+++ UnEarthed_Series/4_UnEarthed_4/server/controllers/gifts.js
@@ -0,0 +1,110 @@
+import { pool } from "../config/database.js";
+
+const getGifts = async (req, res) => {
+  try {
+    const results = await pool.query("SELECT * FROM gifts ORDER BY id ASC");
+    res.status(200).json(results.rows);
+  } catch (err) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const getGiftById = async (req, res) => {
+  try {
+    const selectQuery = `
+      SELECT name, pricePoint, audience, image, description, submittedBy, submittedOn
+      FROM gifts
+      WHERE id=$1
+    `;
+    const giftId = req.params.giftId;
+
+    const result = await pool.query(selectQuery, [giftId]);
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const createGift = async (req, res) => {
+  try {
+    const {
+      name,
+      pricepoint,
+      audience,
+      image,
+      description,
+      submittedby,
+      submittedon,
+    } = req.body;
+    const query = `INSERT INTO gifts (name, pricepoint, audience, image, description,
+    submittedby, submittedon)
+    VALUES($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *`;
+    const attributesList = [
+      name,
+      pricepoint,
+      audience,
+      image,
+      description,
+      submittedby,
+      submittedon,
+    ];
+    const results = await pool.query(query, attributesList);
+    res.status(201).json(results.rows[0]); //append to the very top
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const updateGift = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const {
+      name,
+      pricepoint,
+      audience,
+      image,
+      description,
+      submittedby,
+      submittedon,
+    } = req.body;
+    const query =
+      "UPDATE gifts SET name = $1, pricepoint = $2, audience = $3, image = $4, description = $5, submittedby = $6, submittedon = $7 WHERE id = $8";
+    const attributesList = [
+      name,
+      pricepoint,
+      audience,
+      image,
+      description,
+      submittedby,
+      submittedon,
+      id,
+    ];
+    const result = await pool.query(query, attributesList);
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const deleteGift = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const results = await pool.query("DELETE FROM gifts WHERE id = $1", [id]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+export default {
+  getGifts,
+  getGiftById,
+
+  updateGift,
+  createGift,
+  deleteGift,
+};
+
+//export default GiftsController;
---
+++ UnEarthed_Series/4_UnEarthed_4/server/routes/gifts.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import GiftsController from "../controllers/gifts.js";
+
+
+
+// import { fileURLToPath } from "url";
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const router = express.Router();
+console.log("route running");
+router.get("/", GiftsController.getGifts);
+// router.get("/", (req, res) => {
+//   res.status(200).json(giftData);
+// });
+
+router.get('/:giftId', GiftsController.getGiftById);
+//router.get("/:giftId", (req, res) => {
+  //router.get('/:giftId', GiftsController.getGiftByI)
+  
+  //creating get route
+  //res.status(200).sendFile(path.resolve(__dirname, "../public/gift.html"));
+//});
+
+router.post('/', GiftsController.createGift);
+
+router.delete('/', GiftsController.deleteGift);
+
+router.patch('/:id', GiftsController.updateGift);
+
+export default router;
